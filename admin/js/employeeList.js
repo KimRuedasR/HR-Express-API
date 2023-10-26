cb0ab09d49fc00c7acdd7b19b5d02b68656867bf
KimRuedasR <kruedas09@alumnos.uaq.mx>
2023-10-25 20:56:41 -0600

    🗿 updated namings and variables 🗿

---
+++ admin/js/employeeList.js
@@ -0,0 +1,36 @@
+window.onload = init;
+var headers = {};
+var url = "http://localhost:3000";
+
+function init() {
+  console.log("Token is:", localStorage.getItem("token"));
+  if (localStorage.getItem("token")) {
+    headers = {
+      headers: {
+        Authorization: "bearer " + localStorage.getItem("token"),
+      },
+    };
+    loadEmployees();
+  } else {
+    window.location.href = "index.html";
+  }
+}
+
+function loadEmployees() {
+  axios
+    .get(url + "/employeeRoutes", headers)
+    .then(function (res) {
+      console.log(res);
+      displayEmployees(res.data.message);
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
+}
+
+function displayEmployees(employees) {
+  var body = document.querySelector("body");
+  for (var i = 0; i < employees.length; i++) {
+    body.innerHTML += `<h3>${employees[i].emp_nombre} ${employees[i].emp_apellidos}</h3>`;
+  }
+}
---
+++ admin/js/login.js
@@ -0,0 +1,41 @@
+window.onload = init;
+
+function init() {
+  if (!localStorage.getItem("token")) {
+    document
+      .querySelector(".btn-secondary")
+      .addEventListener("click", function () {
+        window.location.href = "signin.html";
+      });
+
+    document.querySelector(".btn-primary").addEventListener("click", login);
+  } else {
+    window.location.href = "empleados.html";
+  }
+}
+
+function login() {
+  var mail = document.getElementById("input-mail").value;
+  var pass = document.getElementById("input-password").value;
+
+  axios({
+    method: "post",
+    url: "http://localhost:3000/user/login",
+    data: {
+      admin_correo: mail,
+      admin_contrasena: pass,
+    },
+  })
+    .then(function (res) {
+      if (res.data.code === 200) {
+        localStorage.setItem("token", res.data.message);
+        window.location.href = "empleados.html";
+      } else {
+        alert("Usuario y/o contraseña incorrectos");
+      }
+    })
+    .catch(function (err) {
+      console.log("Ocurrio un error:", err);
+      alert("Ocurrio un error, por favor revisa la consola por los detalles.");
+    });
+}
