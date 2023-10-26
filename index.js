dd7c40e15056b8989c28a7b6bceba5efb647cffb
KimRuedasR <kruedas09@alumnos.uaq.mx>
2023-10-25 23:21:46 -0600

    🗿 feat(Index): add Cool logo 🗿

--- index.js
+++ index.js
@@ -27,7 +27,24 @@ app.use("/employees", employeeRoutes);
 app.use(notFound);
 
 app.listen(process.env.PORT || 3000, () => {
-  console.log("--------------------------------");
-  console.log("----::Servidor RH iniciado::----");
-  console.log("--------------------------------");
+  console.log("--------------------------------------------------------------");
+  console.log("|                                                            |");
+  console.log("|               Taller de Node.js S.A. de C.V.               |");
+  console.log("|                           v1.1.0                           |");
+  console.log("|                                                            |");
+  console.log("|                                                            |");
+  console.log("|                  HHHH H   HHHH HH   RRRRRRR R              |");
+  console.log("|                  HHHH H   HHHH HH   RRRRRRR RR             |");
+  console.log("|                  HHHH H   HHHH HH   RRR   RR RR            |");
+  console.log("|                  HHHH H   HHHH HH   RRR    RR RR           |");
+  console.log("|                  HHHHHHHHHHHHH HH   RRR   RR RR            |");
+  console.log("|                  HHHHHHHHHHHHH HH   RRRRRRR RR             |");
+  console.log("|                  HHHH H   HHHH HH   RRRRR RR               |");
+  console.log("|                  HHHH H   HHHH HH   RRR RR RR              |");
+  console.log("|                  HHHH H   HHHH HH   RRR  RR RR             |");
+  console.log("|                  HHHH H   HHHH HH   RRR   RR RR            |");
+  console.log("|                                                            |");
+  console.log("|               ----:: Servidor RH iniciado ::----           |");
+  console.log("|                                                            |");
+  console.log("--------------------------------------------------------------");
 });
