08a604a8559f3ea0cf7c77b7180b164807cdb305
KimRuedasR <kruedas09@alumnos.uaq.mx>
2023-10-25 21:28:49 -0600

    "🗿 fix(Interface): fix missing files and naming for graphic interface authentication and element display  🗿"

---
+++ admin/js/signin.js
@@ -0,0 +1,39 @@
+window.onload = init;
+
+function init() {
+  if (!localStorage.getItem("token")) {
+    document
+      .querySelector(".btn-secondary")
+      .addEventListener("click", function () {
+        window.location.href = "login.html";
+      });
+
+    document.querySelector(".btn-primary").addEventListener("click", signin);
+  } else {
+    window.location.href = "empleados.html";
+  }
+}
+
+function signin() {
+  var name = document.getElementById("input-name").value;
+  var mail = document.getElementById("input-mail").value;
+  var pass = document.getElementById("input-password").value;
+
+  axios({
+    method: "post",
+    url: "http://localhost:3000/user/signin",
+    data: {
+      admin_nombre: name,
+      admin_correo: mail,
+      admin_contrasena: pass,
+    },
+  })
+    .then(function (res) {
+      console.log(res);
+      alert("Registro exitoso");
+      window.location.href = "login.html";
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
+}
