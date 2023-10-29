6d6480b38abb0edd966d32ffa115b79f2588c864
Kim Ruedas <kruedas09@alumnos.uaq.mx>
2023-10-29 16:21:43 -0600

    🗿 feat(Routes): add Delete operation for Admin entity 🗿

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -52,4 +52,18 @@ admin.get("/", async (req, res, next) => {
   return res.status(200).json({ code: 200, message: rows });
 });
 
+admin.delete("/:id([0-9]{1,3})", async (req, res, next) => {
+  const query = `DELETE FROM administradores WHERE admin_id=${req.params.id};`;
+  const rows = await db.query(query);
+
+  if (rows.affectedRows === 1) {
+    return res
+      .status(200)
+      .json({ code: 200, message: "Administrador eliminado correctamente" });
+  }
+  return res
+    .status(404)
+    .json({ code: 404, message: "Administrador no encontrado" });
+});
+
 module.exports = admin;
