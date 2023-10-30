7c4849ce731f8e55b5f753b2a26475adf2f1e400
Kim Ruedas <kruedas09@alumnos.uaq.mx>
2023-10-29 20:46:30 -0600

    🗿 feat(Employees): add Frontend interface for  displaying and creating employees 🗿

--- admin/js/employeeList.js
+++ admin/js/employeeList.js
@@ -29,8 +29,39 @@ function loadEmployees() {
 }
 
 function displayEmployees(employees) {
-  var body = document.querySelector("body");
+  var tbody = document.querySelector("tbody");
+  var table = document.querySelector("table");
+  var row = document.createElement("tr");
+
   for (var i = 0; i < employees.length; i++) {
-    body.innerHTML += `<h3>${employees[i].emp_nombre} ${employees[i].emp_apellidos}</h3>`;
+    var employee = employees[i];
+    var row = document.createElement("tr");
+
+    var id = document.createElement("td");
+    id.textContent = employee.emp_id;
+    var Nombre = document.createElement("td");
+    Nombre.textContent = employee.emp_nombre;
+    var Apellido = document.createElement("td");
+    Apellido.textContent = employee.emp_apellidos;
+    var Telefono = document.createElement("td");
+    Telefono.textContent = employee.emp_telefono;
+    var Correo = document.createElement("td");
+    Correo.textContent = employee.emp_correo;
+    var Direccion = document.createElement("td");
+    Direccion.textContent = employee.emp_direccion;
+
+    row.appendChild(id);
+    row.appendChild(Nombre);
+    row.appendChild(Apellido);
+    row.appendChild(Telefono);
+    row.appendChild(Correo);
+    row.appendChild(Direccion);
+
+    tbody.appendChild(row);
   }
+
+  table.appendChild(tbody);
+
+  var body = document.querySelector("body");
+  body.appendChild(table);
 }
