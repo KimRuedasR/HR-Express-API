525a915f0f94f0ec50d48e97760707ebc4018064
Kim Ruedas <kruedas09@alumnos.uaq.mx>
2023-10-29 18:01:07 -0600

    🗿 feat(Routes): add Patch and Put operations for Admin entity 🗿

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -52,6 +52,64 @@ admin.get("/", async (req, res, next) => {
   return res.status(200).json({ code: 200, message: rows });
 });
 
+admin.patch("/:id([0-9]{1,3})", async (req, res, next) => {
+  const { admin_nombre, admin_correo, admin_contrasena } = req.body;
+
+  let query = "UPDATE administradores SET ";
+  const params = [];
+
+  if (admin_nombre) {
+    params.push(`admin_nombre='${admin_nombre}'`);
+  }
+  if (admin_correo) {
+    params.push(`admin_correo='${admin_correo}'`);
+  }
+  if (admin_contrasena) {
+    params.push(`admin_contrasena='${admin_contrasena}'`);
+  }
+
+  if (params.length === 0) {
+    return res.status(400).json({ code: 400, message: "Campos incompletos" });
+  }
+
+  query += params.join(", ");
+  query += ` WHERE admin_id=${req.params.id};`;
+
+  const rows = await db.query(query);
+
+  if (rows.affectedRows === 1) {
+    return res
+      .status(200)
+      .json({ code: 200, message: "Administrador actualizado correctamente" });
+  }
+
+  return res
+    .status(500)
+    .json({ code: 500, message: "Ocurrió un error en la actualización" });
+});
+
+admin.put("/:id([0-9]{1,3})", async (req, res, next) => {
+  const { admin_nombre, admin_correo, admin_contrasena } = req.body;
+
+  if (!admin_nombre || !admin_correo || !admin_contrasena) {
+    return res.status(400).json({ code: 400, message: "Campos incompletos" });
+  }
+
+  let query = `UPDATE administradores SET admin_nombre='${admin_nombre}', admin_correo='${admin_correo}', admin_contrasena='${admin_contrasena}' WHERE admin_id=${req.params.id};`;
+
+  const rows = await db.query(query);
+
+  if (rows.affectedRows === 1) {
+    return res
+      .status(200)
+      .json({ code: 200, message: "Administrador actualizado correctamente" });
+  }
+
+  return res
+    .status(500)
+    .json({ code: 500, message: "Ocurrió un error en la actualización" });
+});
+
 admin.delete("/:id([0-9]{1,3})", async (req, res, next) => {
   const query = `DELETE FROM administradores WHERE admin_id=${req.params.id};`;
   const rows = await db.query(query);
